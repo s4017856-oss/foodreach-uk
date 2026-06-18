fb6662bfb21b116e6707cb67ffdd4bd3e0e33486
Shikha Agarwal <s4017856@gmail.com>
2026-06-18 12:13:53 +0100

    fix QR code URL

--- pages/3_Your_Voucher.py
+++ pages/3_Your_Voucher.py
@@ -56,7 +56,7 @@ else:
     expiry_str = expiry.strftime("%d %b %Y")
 
     # Generate QR code
-    qr_data = f"CRISISCONNECT|{v['voucher_id']}|{v['postcode']}|{v['total_people']}|{v['issued_at']}"
+    qr_data = f"https://foodreach-uk.streamlit.app/Your_Voucher"
     qr = qrcode.QRCode(version=1, box_size=10, border=4)
     qr.add_data(qr_data)
     qr.make(fit=True)
